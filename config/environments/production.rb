fc85faf8807582a009ac0726930feca33b4ec606
Daniel Alejandro Dominguez Diaz <danielfromarg@gmail.com>
2026-01-25 23:21:56 -0300

    prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,5 +87,5 @@ Rails.application.configure do
   config.hosts << ENV.fetch("APP_HOST", "vulnsentry.com")
   #
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
