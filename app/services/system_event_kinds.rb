4e467cbfc387e5fbb3f3dac39c9e8cfe65f6741b
Daniel Alejandro Dominguez Diaz <danielfromarg@gmail.com>
2026-01-28 20:00:42 -0300

    fix kind

---
+++ app/services/system_event_kinds.rb
@@ -0,0 +1,19 @@
+class SystemEventKinds
+  # Return an array of distinct, non-empty kinds sorted alphabetically.
+  # Cached in-memory for the process; call .clear_cache! to refresh.
+  def self.all
+    @kinds ||= begin
+      SystemEvent.where.not(kind: [nil, ""]).distinct.pluck(:kind).compact.map(&:to_s).sort
+    rescue StandardError
+      []
+    end
+  end
+
+  def self.options_for_select
+    all.map { |k| [k, k] }
+  end
+
+  def self.clear_cache!
+    @kinds = nil
+  end
+end
